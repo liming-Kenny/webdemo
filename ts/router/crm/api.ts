5ce9494beed87c8378a1b423d8eacbd276df843b
liny <670904107@qq.com>
2017-07-10 20:10:24 +0800

    update

--- ts/router/crm/api.ts
+++ ts/router/crm/api.ts
@@ -12,7 +12,8 @@ router.handle("get", "/all", async (ctx, next) => {
 
 router.handle("post", "/add", async (ctx, next) => {
     console.log(ctx)
-    const { name, host, port, type, group, args } = (ctx.request as any).body
+    const { id, name, host, port, type, group, args } = (ctx.request as any).body
+
     let obj = {
         name: name,
         host: host,
@@ -21,12 +22,32 @@ router.handle("post", "/add", async (ctx, next) => {
         group: group,
         args: args,
     }
-    let res = await api.getInstance().insertOne(obj as API)
-    ctx.body = res;
+
+    if (!!id) {
+        obj = { ...obj, id: id }
+    }
+
+    try {
+        await api.getInstance().insertOne(obj as API)
+        ctx.body = "success";
+    } catch (e) {
+        ctx.body = "faild"
+    }
 })
 
 router.handle("get", "/group/:group", async (ctx, next) => {
     let group = ctx.params.group
     let res = await api.getInstance().findByGroup(group);
     ctx.body = res;
+})
+
+router.handle("delete", "/delete/:id", async (ctx, next) => {
+    let id = ctx.params.id
+
+    try {
+        await api.getInstance().deleteById(id);
+        ctx.body = "success";
+    } catch (e) {
+        ctx.body = "faild"
+    }
 })
